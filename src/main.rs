9d3afeb55e59a4dd3ada8a217d1fef5d54f589a0
Matej Marinko <matej@zerodays.dev>
2022-12-20 18:50:56 +0100

    fix main

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,70 @@
-// mod day01;
-// mod day02;
-// mod day03;
-// mod day04;
-// mod day05;
-// mod day06;
-// mod day07;
-// mod day08;
-// mod day09;
-// mod day10;
-// mod day11;
-// mod day12;
-// mod day13;
-// mod day14;
-// mod day15;
-// mod day16;
-// mod day17;
-// mod day18;
-// mod day19;
+use std::env;
+
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
+mod day10;
+mod day11;
+mod day12;
+mod day13;
+mod day14;
+mod day15;
+mod day16;
+mod day17;
+mod day18;
+mod day19;
 mod day20;
 
+const USAGE : &str = "Usage:\ncargo run --release DAY\n\tor./advent-of-code-2022 DAY\n\nExample:./advent-of-code-2022 13 # run day 13";
+
+fn print_usage() {
+    println!("{}", USAGE);
+}
+
+fn run(day: u32) {
+    match day {
+        1 => day01::day01(),
+        2 => day02::day02(),
+        3 => day03::day03(),
+        4 => day04::day04(),
+        5 => day05::day05(),
+        6 => day06::day06(),
+        7 => day07::day07(),
+        8 => day08::day08(),
+        9 => day09::day09(),
+        10 => day10::day10(),
+        11 => day11::day11(),
+        12 => day12::day12(),
+        13 => day13::day13(),
+        14 => day14::day14(),
+        15 => day15::day15(),
+        16 => day16::day16(),
+        17 => day17::day17(),
+        18 => day18::day18(),
+        19 => day19::day19(),
+        20 => day20::day20(),
+        21 => todo!(),
+        22 => todo!(),
+        23 => todo!(),
+        24 => todo!(),
+        25 => todo!(),
+        _ => panic!("Invalid day specified"),
+    }
+}
+
 fn main() {
-    // println!("Day 01");
-    // day01::day01();
+    let args: Vec<String> = env::args().collect();
 
-    // println!("\nDay 02");
-    // day02::day02();
-
-    // println!("\nDay 03");
-    // day03::day03();
-
-    // println!("\nDay 04");
-    // day04::day04();
-
-    // println!("\nDay 05");
-    // day05::day05();
-
-    // println!("\nDay 06");
-    // day06::day06();
-
-    // println!("\nDay 07");
-    // day07::day07();
-
-    // println!("\nDay 08");
-    // day08::day08();
-
-    // println!("\nDay 09");
-    // day09::day09();
-
-    // println!("\nDay 10");
-    // day10::day10();
-
-    // println!("\nDay 11");
-    // day11::day11();
-
-    // println!("\nDay 12");
-    // day12::day12();
-
-    // println!("\nDay 13");
-    // day13::day13();
-
-    // println!("\nDay 14");
-    // day14::day14();
-
-    // println!("\nDay 15");
-    // day15::day15();
-
-    // println!("\nDay 16");
-    // day16::day16();
-
-    // println!("\nDay 17");
-    // day17::day17();
-
-    // println!("\nDay 18");
-    // day18::day18();
-
-    // println!("\nDay 19");
-    // day19::day19();
-
-    println!("\nDay 20");
-    day20::day20();
+    if args.len() != 2 {
+        print_usage();
+    } else {
+        let day = args[1].parse::<u32>().expect(USAGE);
+        run(day);
+    }
 }
